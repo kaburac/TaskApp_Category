c2fff363617406bab5b8e35b4e31b42d31cdc2df
kaburac <kaburac@cocoto.co.jp>
2020-10-19 14:42:35 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/chika/kaburagi/taskapp/TaskAdapter.kt
@@ -0,0 +1,43 @@
+package jp.techacademy.chika.kaburagi.taskapp
+
+import android.R
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
+
+class TaskAdapter(context: Context): BaseAdapter() {
+    private val mLayoutInflater: LayoutInflater = LayoutInflater.from(context)
+    var taskList = mutableListOf<Task>()
+
+    override fun getCount(): Int {
+        return taskList.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return taskList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return taskList[position].id.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View = convertView ?: mLayoutInflater.inflate(android.R.layout.simple_list_item_2, null)
+
+        val textView1 = view.findViewById<TextView>(android.R.id.text1)
+        val textView2 = view.findViewById<TextView>(android.R.id.text2)
+
+        textView1.text = taskList[position].title
+
+        val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.JAPANESE)
+        val date = taskList[position].date
+        textView2.text = simpleDateFormat.format(date)
+
+        return view
+    }
+}
